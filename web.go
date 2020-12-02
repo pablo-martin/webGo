4172cec3fa845c8ff28408e19a151db45d71d76f
Pablo Martin <elektrochose@gmail.com>
2020-12-01 20:03:13 -0600

    static pages working now

--- web.go
+++ web.go
@@ -1,107 +1,32 @@
 package main
 
 import (
-    "log"
-	"net/http"
-	"io/ioutil"
 	"html/template"
-	"regexp"
-	"errors"
-	"fmt"
+	"log"
+	"net/http"
 )
 
-var templates = template.Must(template.ParseFiles("templates/view.html", "templates/edit.html"))
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var templates = template.Must(template.ParseFiles("templates/view.html", "templates/edit.html", "templates/books.html"))
 
 func main() {
-	
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-    log.Fatal(http.ListenAndServe(":8080", nil))
+
+	http.HandleFunc("/books/", bookHandler)
+	http.HandleFunc("/hypermaze/", mazeHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-type Page struct {
-    Title string
-    Body  []byte
+func mazeHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "edit")
 }
 
-func loadPage(title string) (*Page, error) {
-    filename := "./data/" + title + ".txt"
-    body, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
+func bookHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "books")
 }
 
-func (p *Page) save() error {
-    filename := "./data/" + p.Title + ".txt"
-    return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func makeHandler(fn func (http.ResponseWriter, *http.Request, string)) http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request){		
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil{
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
-}
-
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string){
-	p, err := loadPage(title)
-	if err != nil{ 
-        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-        return
-    }
-	renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string){
-	p, err := loadPage(title)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", nil)
 	if err != nil {
-        p = &Page{Title: title}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-	renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string){
-	body := r.FormValue("body")
-	p := Page{Title: title, Body: []byte(body)}
-	err := p.save()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-}
-
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error){
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil{
-		http.NotFound(w, r)
-		return "", errors.New("Invalid Page Title")
-	}
-	return m[2], nil
-}
-
-func getTemplates(dir string){
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {log.Fatal(err)}
-
-	for _, f := range files {
-		fmt.Println(f.Name())
-	}
-
 }
